9c0e79d63924e182ab7b47c5e886021896ffde95
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-01-30 15:58:23 +0800

    [ThinkCode] #0 修改divider示例

--- design/src/main/java/com/think/design/divider/DividerFragment.kt
+++ design/src/main/java/com/think/design/divider/DividerFragment.kt
@@ -1,97 +1,68 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.think.design.divider
 
-package com.think.design.divider;
-
-import com.think.design.R;
-
-import androidx.fragment.app.Fragment;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import dagger.Provides;
-import dagger.android.ContributesAndroidInjector;
-import dagger.multibindings.IntoSet;
-import com.think.design.application.scope.ActivityScope;
-import com.think.design.application.scope.FragmentScope;
-import com.think.design.feature.Demo;
-import com.think.design.feature.DemoLandingFragment;
-import com.think.design.feature.FeatureDemo;
-import java.util.ArrayList;
-import java.util.List;
+import androidx.fragment.app.Fragment
+import com.think.design.R
+import com.think.design.application.scope.ActivityScope
+import com.think.design.application.scope.FragmentScope
+import com.think.design.feature.Demo
+import com.think.design.feature.DemoLandingFragment
+import com.think.design.feature.FeatureDemo
+import dagger.Provides
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoSet
 
 /**
  * A landing fragment that links to demos of the divider.
  */
-public class DividerFragment extends DemoLandingFragment {
+class DividerFragment : DemoLandingFragment() {
 
-  @Override
-  public int getTitleResId() {
-    return R.string.cat_divider_demo_title;
-  }
-
-  @Override
-  public int getDescriptionResId() {
-    return R.string.cat_divider_description;
-  }
-
-  @NonNull
-  @Override
-  public Demo getMainDemo() {
-    return new Demo() {
-      @Override
-      public Fragment createFragment() {
-        return new DividerMainDemoFragment();
-      }
-    };
-  }
-
-  @Override
-  @NonNull
-  public List<Demo> getAdditionalDemos() {
-    List<Demo> additionalDemos = new ArrayList<>();
-    additionalDemos.add(
-        new Demo(R.string.cat_divider_item_decoration_demo_title) {
-          @Nullable
-          @Override
-          public Fragment createFragment() {
-            return new DividerItemDecorationDemoFragment();
-          }
-        }
-    );
-    return additionalDemos;
-  }
-
-  /** The Dagger module for {@link DividerFragment} dependencies. */
-  @dagger.Module
-  public abstract static class Module {
-
-    @FragmentScope
-    @ContributesAndroidInjector
-    abstract DividerFragment contributeInjector();
-
-    @IntoSet
-    @Provides
-    @ActivityScope
-    static FeatureDemo provideFeatureDemo() {
-      return new FeatureDemo(R.string.cat_divider_demo_title, R.drawable.ic_placeholder) {
-        @Override
-        public Fragment createFragment() {
-          return new DividerFragment();
-        }
-      };
+    override fun getTitleResId(): Int {
+        return R.string.cat_divider_demo_title
     }
-  }
+
+    override fun getDescriptionResId(): Int {
+        return R.string.cat_divider_description
+    }
+
+    override fun getMainDemo(): Demo {
+        return object : Demo() {
+            override fun createFragment(): Fragment {
+                return DividerMainDemoFragment()
+            }
+        }
+    }
+
+    override fun getAdditionalDemos(): MutableList<Demo?> {
+        val additionalDemos: MutableList<Demo?> = ArrayList()
+        additionalDemos.add(object : Demo(R.string.cat_divider_item_decoration_demo_title) {
+            override fun createFragment(): Fragment {
+                return DividerItemDecorationDemoFragment()
+            }
+        })
+        return additionalDemos
+    }
+
+    /** The Dagger module for [DividerFragment] dependencies.  */
+
+
+    @dagger.Module
+    abstract class Module {
+        @FragmentScope
+        @ContributesAndroidInjector
+        abstract fun contributeInjector(): DividerFragment?
+
+        companion object {
+            @IntoSet
+            @Provides
+            @ActivityScope
+            fun provideFeatureDemo(): FeatureDemo {
+                return object :
+                    FeatureDemo(R.string.cat_divider_demo_title, R.drawable.ic_placeholder) {
+                    override fun createFragment(): Fragment {
+                        return DividerFragment()
+                    }
+                }
+            }
+        }
+    }
 }
