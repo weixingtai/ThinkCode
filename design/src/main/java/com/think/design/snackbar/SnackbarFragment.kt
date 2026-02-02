0b517b9c1d16b72317c3969702c4aeb52f52f4a5
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-02-02 09:57:54 +0800

    [ThinkCode] #0 修改Snackbar示例

--- design/src/main/java/com/think/design/snackbar/SnackbarFragment.kt
+++ design/src/main/java/com/think/design/snackbar/SnackbarFragment.kt
@@ -1,74 +1,53 @@
-/*
- * Copyright 2025 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.think.design.snackbar
 
-package com.think.design.snackbar;
+import androidx.fragment.app.Fragment
+import com.think.design.R
+import com.think.design.application.scope.ActivityScope
+import com.think.design.application.scope.FragmentScope
+import com.think.design.feature.Demo
+import com.think.design.feature.DemoLandingFragment
+import com.think.design.feature.FeatureDemo
+import dagger.Provides
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoSet
 
-import com.think.design.R;
-
-import androidx.fragment.app.Fragment;
-import dagger.Provides;
-import dagger.android.ContributesAndroidInjector;
-import dagger.multibindings.IntoSet;
-import com.think.design.application.scope.ActivityScope;
-import com.think.design.application.scope.FragmentScope;
-import com.think.design.feature.Demo;
-import com.think.design.feature.DemoLandingFragment;
-import com.think.design.feature.FeatureDemo;
-
-/** A landing fragment that links to Snackbar demos for the Catalog app. */
-public class SnackbarFragment extends DemoLandingFragment {
-
-  @Override
-  public int getTitleResId() {
-    return R.string.cat_snackbar_title;
-  }
-
-  @Override
-  public int getDescriptionResId() {
-    return R.string.cat_snackbar_description;
-  }
-
-  @Override
-  public Demo getMainDemo() {
-    return new Demo() {
-      @Override
-      public Fragment createFragment() {
-        return new SnackbarMainDemoFragment();
-      }
-    };
-  }
-
-  /** The Dagger module for {@link SnackbarFragment} dependencies. */
-  @dagger.Module
-  public abstract static class Module {
-
-    @FragmentScope
-    @ContributesAndroidInjector
-    abstract SnackbarFragment contributeInjector();
-
-    @IntoSet
-    @Provides
-    @ActivityScope
-    static FeatureDemo provideFeatureDemo() {
-      return new FeatureDemo(R.string.cat_snackbar_title, R.drawable.ic_placeholder) {
-        @Override
-        public Fragment createFragment() {
-          return new SnackbarFragment();
-        }
-      };
+class SnackbarFragment : DemoLandingFragment() {
+    override fun getTitleResId(): Int {
+        return R.string.cat_snackbar_title
     }
-  }
+
+    override fun getDescriptionResId(): Int {
+        return R.string.cat_snackbar_description
+    }
+
+    override fun getMainDemo(): Demo {
+        return object : Demo() {
+            override fun createFragment(): Fragment? {
+                return SnackbarMainDemoFragment()
+            }
+        }
+    }
+
+    /** The Dagger module for [SnackbarFragment] dependencies.  */
+    @dagger.Module
+    abstract class Module {
+        @FragmentScope
+        @ContributesAndroidInjector
+        abstract fun contributeInjector(): SnackbarFragment?
+
+        companion object {
+            @JvmStatic
+            @IntoSet
+            @Provides
+            @ActivityScope
+            fun provideFeatureDemo(): FeatureDemo {
+                return object :
+                    FeatureDemo(R.string.cat_snackbar_title, R.drawable.ic_placeholder) {
+                    override fun createFragment(): Fragment {
+                        return SnackbarFragment()
+                    }
+                }
+            }
+        }
+    }
 }
--- design/src/main/java/com/think/design/snackbar/SnackbarMainDemoFragment.kt
+++ design/src/main/java/com/think/design/snackbar/SnackbarMainDemoFragment.kt
@@ -1,144 +1,136 @@
-/*
- * Copyright 2025 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.think.design.snackbar
 
-package com.think.design.snackbar;
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
+import com.think.design.R
+import com.think.design.feature.DemoFragment
 
-import com.think.design.R;
+class SnackbarMainDemoFragment : DemoFragment() {
+    override fun onCreateDemoView(
+        layoutInflater: LayoutInflater, viewGroup: ViewGroup?, bundle: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.cat_snackbar_main_demo_fragment, viewGroup, false)
+    }
 
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.ContextThemeWrapper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import com.google.android.material.snackbar.Snackbar;
-import com.think.design.feature.DemoFragment;
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coordinatorLayout = view.findViewById<CoordinatorLayout>(R.id.coordinator_layout)
 
-/** A fragment that displays the main Snackbar demos for the Catalog app. */
-public class SnackbarMainDemoFragment extends DemoFragment {
+        // Default snackbar
+        view.findViewById<View?>(R.id.default_button)!!
+            .setOnClickListener(
+                View.OnClickListener { v: View? ->
+                    Snackbar.make(
+                        coordinatorLayout,
+                        R.string.cat_snackbar_default_message,
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .show()
+                })
 
-  @Override
-  public View onCreateDemoView(
-      LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
-    return layoutInflater.inflate(R.layout.cat_snackbar_main_demo_fragment, viewGroup, false);
-  }
+        // Snackbar with single line
+        view.findViewById<View?>(R.id.single_line_button)!!
+            .setOnClickListener(
+                View.OnClickListener { v: View? ->
+                    Snackbar.make(
+                        coordinatorLayout,
+                        R.string.cat_snackbar_single_line_message,
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .setTextMaxLines(1)
+                        .show()
+                })
 
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    CoordinatorLayout coordinatorLayout = view.findViewById(R.id.coordinator_layout);
+        // Snackbar with action
+        view.findViewById<View?>(R.id.with_action_button)!!
+            .setOnClickListener(
+                View.OnClickListener { v: View? ->
+                    Snackbar.make(
+                        coordinatorLayout,
+                        R.string.cat_snackbar_with_action_message,
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .setAction(
+                            R.string.cat_snackbar_action_title,
+                            View.OnClickListener { a: View? -> })
+                        .show()
+                })
 
-    // Default snackbar
-    view.findViewById(R.id.default_button)
-        .setOnClickListener(
-            v -> {
-              Snackbar.make(
-                      coordinatorLayout,
-                      R.string.cat_snackbar_default_message,
-                      Snackbar.LENGTH_SHORT)
-                  .show();
-            });
+        // Snackbar with multiple lines
+        view.findViewById<View?>(R.id.multi_line_button)!!
+            .setOnClickListener(
+                View.OnClickListener { v: View? ->
+                    Snackbar.make(
+                        coordinatorLayout,
+                        R.string.cat_snackbar_multi_line_message,
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .setAction(
+                            R.string.cat_snackbar_action_title,
+                            View.OnClickListener { a: View? -> })
+                        .setTextMaxLines(5)
+                        .setCloseIconVisible(true)
+                        .show()
+                })
 
-    // Snackbar with single line
-    view.findViewById(R.id.single_line_button)
-        .setOnClickListener(
-            v -> {
-              Snackbar.make(
-                      coordinatorLayout,
-                      R.string.cat_snackbar_single_line_message,
-                      Snackbar.LENGTH_SHORT)
-                  .setTextMaxLines(1)
-                  .show();
-            });
+        // Snackbar with custom shape
+        view.findViewById<View?>(R.id.custom_shape_button)!!
+            .setOnClickListener(
+                View.OnClickListener { v: View? ->
+                    // Set a custom shape by updating the theme's snackbarStyle attribute to a
+                    // custom style with a custom shapeAppearance. For demonstration, this is done
+                    // on a single snackbar using a theme overlay but it is more typically done by
+                    // setting the snackbarStyle on th main theme and customizing all snackbars app-wide.
+                    val c: Context =
+                        ContextThemeWrapper(
+                            v!!.getContext(), R.style.ThemeOverlay_Catalog_SnackbarWithCustomShape
+                        )
+                    val snackbar = Snackbar.make(
+                        c,
+                        coordinatorLayout,
+                        getString(R.string.cat_snackbar_custom_shape_message),
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .setAction("Done", View.OnClickListener { a: View? -> })
+                        .setCloseIconVisible( /* visible= */true)
+                        .setCloseIconResource(R.drawable.ic_cancel_24)
+                        .setCloseIconTint(Color.GREEN)
+                    // Setting the close icon to visible removes the snackbar layout's end padding. To
+                    // customize this, get the snackbar's view and set the end padding to a value
+                    // that fits nicely with the custom shape and icon.
+                    val snackbarLayout = snackbar.getView()
+                    snackbarLayout.setPaddingRelative(
+                        snackbarLayout.getPaddingStart(),
+                        snackbarLayout.getPaddingTop(),
+                        getResources().getDimensionPixelSize(
+                            R.dimen.cat_snackbar_custom_shape_end_padding
+                        ),
+                        snackbarLayout.getPaddingBottom()
+                    )
+                    snackbar.show()
+                })
 
-    // Snackbar with action
-    view.findViewById(R.id.with_action_button)
-        .setOnClickListener(
-            v -> {
-              Snackbar.make(
-                      coordinatorLayout,
-                      R.string.cat_snackbar_with_action_message,
-                      Snackbar.LENGTH_SHORT)
-                  .setAction(R.string.cat_snackbar_action_title, a -> {})
-                  .show();
-            });
-
-    // Snackbar with multiple lines
-    view.findViewById(R.id.multi_line_button)
-        .setOnClickListener(
-            v -> {
-              Snackbar.make(
-                      coordinatorLayout,
-                      R.string.cat_snackbar_multi_line_message,
-                      Snackbar.LENGTH_SHORT)
-                  .setAction(R.string.cat_snackbar_action_title, a -> {})
-                  .setTextMaxLines(5)
-                  .setCloseIconVisible(true)
-                  .show();
-            });
-
-    // Snackbar with custom shape
-    view.findViewById(R.id.custom_shape_button)
-        .setOnClickListener(
-            v -> {
-              // Set a custom shape by updating the theme's snackbarStyle attribute to a
-              // custom style with a custom shapeAppearance. For demonstration, this is done
-              // on a single snackbar using a theme overlay but it is more typically done by
-              // setting the snackbarStyle on th main theme and customizing all snackbars app-wide.
-              Context c =
-                  new ContextThemeWrapper(
-                      v.getContext(), R.style.ThemeOverlay_Catalog_SnackbarWithCustomShape);
-              Snackbar snackbar = Snackbar.make(
-                      c,
-                      coordinatorLayout,
-                      getString(R.string.cat_snackbar_custom_shape_message),
-                      Snackbar.LENGTH_SHORT)
-                  .setAction("Done", a -> {})
-                  .setCloseIconVisible(/* visible= */ true)
-                  .setCloseIconResource(R.drawable.ic_cancel_24)
-                  .setCloseIconTint(Color.GREEN);
-              // Setting the close icon to visible removes the snackbar layout's end padding. To
-              // customize this, get the snackbar's view and set the end padding to a value
-              // that fits nicely with the custom shape and icon.
-              View snackbarLayout = snackbar.getView();
-              snackbarLayout.setPaddingRelative(
-                  snackbarLayout.getPaddingStart(),
-                  snackbarLayout.getPaddingTop(),
-                  getResources().getDimensionPixelSize(
-                      R.dimen.cat_snackbar_custom_shape_end_padding),
-                  snackbarLayout.getPaddingBottom()
-              );
-              snackbar.show();
-            });
-
-    // Snackbar with close icon
-    view.findViewById(R.id.with_close_button)
-        .setOnClickListener(
-            v -> {
-              Snackbar.make(
-                      coordinatorLayout,
-                      R.string.cat_snackbar_with_close_message,
-                      Snackbar.LENGTH_INDEFINITE)
-                  .setAction(R.string.cat_snackbar_action_title, a -> {})
-                  .setCloseIconVisible(true)
-                  .show();
-            });
-  }
+        // Snackbar with close icon
+        view.findViewById<View?>(R.id.with_close_button)!!
+            .setOnClickListener(
+                View.OnClickListener { v: View? ->
+                    Snackbar.make(
+                        coordinatorLayout,
+                        R.string.cat_snackbar_with_close_message,
+                        Snackbar.LENGTH_INDEFINITE
+                    )
+                        .setAction(
+                            R.string.cat_snackbar_action_title,
+                            View.OnClickListener { a: View? -> })
+                        .setCloseIconVisible(true)
+                        .show()
+                })
+    }
 }
