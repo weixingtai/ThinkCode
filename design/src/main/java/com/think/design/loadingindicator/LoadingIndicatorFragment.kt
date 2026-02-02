28918b0985e3a81efad8a34dd447f4d28a08992f
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-02-02 09:54:41 +0800

    [ThinkCode] #0 修改loadingIndicator示例

--- design/src/main/java/com/think/design/loadingindicator/LoadingIndicatorFragment.kt
+++ design/src/main/java/com/think/design/loadingindicator/LoadingIndicatorFragment.kt
@@ -1,74 +1,54 @@
-/*
- * Copyright 2024 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.think.design.loadingindicator;
+package com.think.design.loadingindicator
 
-import com.think.design.R;
+import androidx.fragment.app.Fragment
+import com.think.design.R
+import com.think.design.application.scope.ActivityScope
+import com.think.design.application.scope.FragmentScope
+import com.think.design.feature.Demo
+import com.think.design.feature.DemoLandingFragment
+import com.think.design.feature.FeatureDemo
+import dagger.Provides
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoSet
 
-import androidx.fragment.app.Fragment;
-import androidx.annotation.NonNull;
-import dagger.Provides;
-import dagger.android.ContributesAndroidInjector;
-import dagger.multibindings.IntoSet;
-import com.think.design.application.scope.ActivityScope;
-import com.think.design.application.scope.FragmentScope;
-import com.think.design.feature.Demo;
-import com.think.design.feature.DemoLandingFragment;
-import com.think.design.feature.FeatureDemo;
-
-/** A fragment that displays loading indicator demos for the Catalog app. */
-public class LoadingIndicatorFragment extends DemoLandingFragment {
-  @Override
-  public int getTitleResId() {
-    return R.string.cat_loading_indicator_title;
-  }
-
-  @Override
-  public int getDescriptionResId() {
-    return R.string.cat_loading_indicator_description;
-  }
-
-  @Override
-  @NonNull
-  public Demo getMainDemo() {
-    return new Demo() {
-      @Override
-      public Fragment createFragment() {
-        return new LoadingIndicatorMainDemoFragment();
-      }
-    };
-  }
-
-  /** The Dagger module for {@link LoadingIndicatorFragment} dependencies. */
-  @dagger.Module
-  public abstract static class Module {
-    @FragmentScope
-    @ContributesAndroidInjector
-    abstract LoadingIndicatorFragment contributeInjector();
-
-    @IntoSet
-    @Provides
-    @ActivityScope
-    static FeatureDemo provideFeatureDemo() {
-      return new FeatureDemo(
-          R.string.cat_loading_indicator_title, R.drawable.ic_progress_activity_24px) {
-        @Override
-        public Fragment createFragment() {
-          return new LoadingIndicatorFragment();
-        }
-      };
+class LoadingIndicatorFragment : DemoLandingFragment() {
+    override fun getTitleResId(): Int {
+        return R.string.cat_loading_indicator_title
     }
-  }
+
+    override fun getDescriptionResId(): Int {
+        return R.string.cat_loading_indicator_description
+    }
+
+    override fun getMainDemo(): Demo {
+        return object : Demo() {
+            override fun createFragment(): Fragment {
+                return LoadingIndicatorMainDemoFragment()
+            }
+        }
+    }
+
+    /** The Dagger module for [LoadingIndicatorFragment] dependencies.  */
+    @dagger.Module
+    abstract class Module {
+        @FragmentScope
+        @ContributesAndroidInjector
+        abstract fun contributeInjector(): LoadingIndicatorFragment?
+
+        companion object {
+            @JvmStatic
+            @IntoSet
+            @Provides
+            @ActivityScope
+            fun provideFeatureDemo(): FeatureDemo {
+                return object : FeatureDemo(
+                    R.string.cat_loading_indicator_title, R.drawable.ic_progress_activity_24px
+                ) {
+                    override fun createFragment(): Fragment {
+                        return LoadingIndicatorFragment()
+                    }
+                }
+            }
+        }
+    }
 }
