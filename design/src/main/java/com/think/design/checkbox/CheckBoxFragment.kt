1fcba1760a6d680a3974a0cc6b4470b28c023d84
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-02-02 15:23:30 +0800

    Revert "[ThinkCode] #0 修改checkbox示例"
    This reverts commit a519fe562e24f71391749a5839fb1e3011509bf7.

--- design/src/main/java/com/think/design/checkbox/CheckBoxFragment.kt
+++ design/src/main/java/com/think/design/checkbox/CheckBoxFragment.kt
@@ -1,52 +1,74 @@
-package com.think.design.checkbox
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import androidx.fragment.app.Fragment
-import com.think.design.R
-import com.think.design.application.scope.ActivityScope
-import com.think.design.application.scope.FragmentScope
-import com.think.design.feature.Demo
-import com.think.design.feature.DemoLandingFragment
-import com.think.design.feature.FeatureDemo
-import dagger.Provides
-import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoSet
+package com.think.design.checkbox;
 
-class CheckBoxFragment : DemoLandingFragment() {
-    override fun getTitleResId(): Int {
-        return R.string.cat_checkbox_title
-    }
+import com.think.design.R;
 
-    override fun getDescriptionResId(): Int {
-        return R.string.cat_checkbox_description
-    }
+import androidx.fragment.app.Fragment;
+import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
+import dagger.multibindings.IntoSet;
+import com.think.design.application.scope.ActivityScope;
+import com.think.design.application.scope.FragmentScope;
+import com.think.design.feature.Demo;
+import com.think.design.feature.DemoLandingFragment;
+import com.think.design.feature.FeatureDemo;
 
-    override fun getMainDemo(): Demo {
-        return object : Demo() {
-            override fun createFragment(): Fragment {
-                return CheckBoxMainDemoFragment()
-            }
+/** A landing fragment that links to Checkbox demos for the Catalog app. */
+public class CheckBoxFragment extends DemoLandingFragment {
+
+  @Override
+  public int getTitleResId() {
+    return R.string.cat_checkbox_title;
+  }
+
+  @Override
+  public int getDescriptionResId() {
+    return R.string.cat_checkbox_description;
+  }
+
+  @Override
+  public Demo getMainDemo() {
+    return new Demo() {
+      @Override
+      public Fragment createFragment() {
+        return new CheckBoxMainDemoFragment();
+      }
+    };
+  }
+
+  /** The Dagger module for {@link CheckBoxFragment} dependencies. */
+  @dagger.Module
+  public abstract static class Module {
+
+    @FragmentScope
+    @ContributesAndroidInjector
+    abstract CheckBoxFragment contributeInjector();
+
+    @IntoSet
+    @Provides
+    @ActivityScope
+    static FeatureDemo provideFeatureDemo() {
+      return new FeatureDemo(R.string.cat_checkbox_title, R.drawable.ic_checkbox) {
+        @Override
+        public Fragment createFragment() {
+          return new CheckBoxFragment();
         }
+      };
     }
-
-    /** The Dagger module for [CheckBoxFragment] dependencies.  */
-    @dagger.Module
-    abstract class Module {
-        @FragmentScope
-        @ContributesAndroidInjector
-        abstract fun contributeInjector(): CheckBoxFragment?
-
-        companion object {
-            @JvmStatic
-            @IntoSet
-            @Provides
-            @ActivityScope
-            fun provideFeatureDemo(): FeatureDemo {
-                return object : FeatureDemo(R.string.cat_checkbox_title, R.drawable.ic_checkbox) {
-                    override fun createFragment(): Fragment {
-                        return CheckBoxFragment()
-                    }
-                }
-            }
-        }
-    }
+  }
 }
--- design/src/main/java/com/think/design/checkbox/CheckBoxMainDemoFragment.kt
+++ design/src/main/java/com/think/design/checkbox/CheckBoxMainDemoFragment.kt
@@ -1,111 +1,128 @@
-package com.think.design.checkbox
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.CompoundButton
-import com.google.android.material.checkbox.MaterialCheckBox
-import com.google.android.material.checkbox.MaterialCheckBox.OnCheckedStateChangedListener
-import com.think.design.R
-import com.think.design.feature.DemoFragment
-import com.think.design.feature.DemoUtils
+package com.think.design.checkbox;
 
-class CheckBoxMainDemoFragment : DemoFragment() {
-    private var isUpdatingChildren = false
+import com.think.design.R;
 
-    override fun onCreateDemoView(
-        layoutInflater: LayoutInflater, viewGroup: ViewGroup?, bundle: Bundle?
-    ): View {
-        val view =
-            layoutInflater.inflate(R.layout.cat_checkbox, viewGroup, false)
-        val toggleContainer = view.findViewById<ViewGroup?>(R.id.checkbox_toggle_container)
-        val toggledCheckBoxes: MutableList<CheckBox> =
-            DemoUtils.findViewsWithType<CheckBox>(toggleContainer, CheckBox::class.java)
-        val allCheckBoxes: MutableList<MaterialCheckBox> =
-            DemoUtils.findViewsWithType<MaterialCheckBox>(view, MaterialCheckBox::class.java)
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.checkbox.MaterialCheckBox.OnCheckedStateChangedListener;
+import com.think.design.feature.DemoFragment;
+import com.think.design.feature.DemoUtils;
+import java.util.List;
 
-        val checkBoxToggle = view.findViewById<CheckBox>(R.id.checkbox_toggle)
-        checkBoxToggle.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
-            for (cb in toggledCheckBoxes) {
-                cb.setEnabled(isChecked)
-            }
-        }
+/** A fragment that displays the main Checkbox demos for the Catalog app. */
+public class CheckBoxMainDemoFragment extends DemoFragment {
 
-        val checkBoxToggleError = view.findViewById<CheckBox>(R.id.checkbox_toggle_error)
-        checkBoxToggleError.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
-            for (cb in allCheckBoxes) {
-                cb.setErrorShown(isChecked)
-            }
-        }
+  private boolean isUpdatingChildren = false;
 
-        val firstChild = view.findViewById<CheckBox>(R.id.checkbox_child_1)
-        firstChild.setChecked(true)
-        val indeterminateContainer =
-            view.findViewById<ViewGroup?>(R.id.checkbox_indeterminate_container)
-        val childrenCheckBoxes: MutableList<CheckBox> =
-            DemoUtils.findViewsWithType<CheckBox>(indeterminateContainer, CheckBox::class.java)
-        val checkBoxParent = view.findViewById<MaterialCheckBox>(R.id.checkbox_parent)
-        val parentOnCheckedStateChangedListener =
-            OnCheckedStateChangedListener { checkBox: MaterialCheckBox?, state: Int ->
-                val isChecked = checkBox!!.isChecked
-                if (state != MaterialCheckBox.STATE_INDETERMINATE) {
-                    isUpdatingChildren = true
-                    for (child in childrenCheckBoxes) {
-                        child.setChecked(isChecked)
-                    }
-                    isUpdatingChildren = false
+  @Override
+  public View onCreateDemoView(
+      LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
+    View view = layoutInflater.inflate(R.layout.cat_checkbox, viewGroup, false /* attachToRoot */);
+    ViewGroup toggleContainer = view.findViewById(R.id.checkbox_toggle_container);
+    List<CheckBox> toggledCheckBoxes = DemoUtils.findViewsWithType(toggleContainer, CheckBox.class);
+    List<MaterialCheckBox> allCheckBoxes =
+        DemoUtils.findViewsWithType(view, MaterialCheckBox.class);
+
+    CheckBox checkBoxToggle = view.findViewById(R.id.checkbox_toggle);
+    checkBoxToggle.setOnCheckedChangeListener(
+        (CompoundButton buttonView, boolean isChecked) -> {
+          for (CheckBox cb : toggledCheckBoxes) {
+            cb.setEnabled(isChecked);
+          }
+        });
+
+    CheckBox checkBoxToggleError = view.findViewById(R.id.checkbox_toggle_error);
+    checkBoxToggleError.setOnCheckedChangeListener(
+        (CompoundButton buttonView, boolean isChecked) -> {
+          for (MaterialCheckBox cb : allCheckBoxes) {
+            cb.setErrorShown(isChecked);
+          }
+        });
+
+    CheckBox firstChild = view.findViewById(R.id.checkbox_child_1);
+    firstChild.setChecked(true);
+    ViewGroup indeterminateContainer = view.findViewById(R.id.checkbox_indeterminate_container);
+    List<CheckBox> childrenCheckBoxes =
+        DemoUtils.findViewsWithType(indeterminateContainer, CheckBox.class);
+    MaterialCheckBox checkBoxParent = view.findViewById(R.id.checkbox_parent);
+    OnCheckedStateChangedListener parentOnCheckedStateChangedListener =
+            (checkBox, state) -> {
+              boolean isChecked = checkBox.isChecked();
+              if (state != MaterialCheckBox.STATE_INDETERMINATE) {
+                isUpdatingChildren = true;
+                for (CheckBox child : childrenCheckBoxes) {
+                  child.setChecked(isChecked);
                 }
-            }
-        checkBoxParent.addOnCheckedStateChangedListener(parentOnCheckedStateChangedListener)
+                isUpdatingChildren = false;
+              }
+            };
+    checkBoxParent.addOnCheckedStateChangedListener(parentOnCheckedStateChangedListener);
 
-        val childOnCheckedStateChangedListener =
-            OnCheckedStateChangedListener { _: MaterialCheckBox?, _: Int ->
-                if (isUpdatingChildren) {
-                    return@OnCheckedStateChangedListener
-                }
-                setParentState(
-                    checkBoxParent, childrenCheckBoxes, parentOnCheckedStateChangedListener
-                )
-            }
+    OnCheckedStateChangedListener childOnCheckedStateChangedListener =
+        (checkBox, state) -> {
+          if (isUpdatingChildren) {
+            return;
+          }
+          setParentState(checkBoxParent, childrenCheckBoxes, parentOnCheckedStateChangedListener);
+        };
 
-        for (child in childrenCheckBoxes) {
-            (child as MaterialCheckBox).addOnCheckedStateChangedListener(
-                    childOnCheckedStateChangedListener
-                )
-        }
-
-        setParentState(checkBoxParent, childrenCheckBoxes, parentOnCheckedStateChangedListener)
-
-        return view
+    for (CheckBox child : childrenCheckBoxes) {
+      ((MaterialCheckBox) child)
+          .addOnCheckedStateChangedListener(childOnCheckedStateChangedListener);
     }
 
-    private fun setParentState(
-        checkBoxParent: MaterialCheckBox,
-        childrenCheckBoxes: MutableList<CheckBox>,
-        parentOnCheckedStateChangedListener: OnCheckedStateChangedListener
-    ) {
-        var allChecked = true
-        var noneChecked = true
-        for (child in childrenCheckBoxes) {
-            if (!child.isChecked) {
-                allChecked = false
-            } else {
-                noneChecked = false
-            }
-            if (!allChecked && !noneChecked) {
-                break
-            }
-        }
-        checkBoxParent.removeOnCheckedStateChangedListener(parentOnCheckedStateChangedListener)
-        if (allChecked) {
-            checkBoxParent.isChecked = true
-        } else if (noneChecked) {
-            checkBoxParent.isChecked = false
-        } else {
-            checkBoxParent.checkedState = MaterialCheckBox.STATE_INDETERMINATE
-        }
-        checkBoxParent.addOnCheckedStateChangedListener(parentOnCheckedStateChangedListener)
+    setParentState(checkBoxParent, childrenCheckBoxes, parentOnCheckedStateChangedListener);
+
+    return view;
+  }
+
+  private void setParentState(
+      @NonNull MaterialCheckBox checkBoxParent,
+      @NonNull List<CheckBox> childrenCheckBoxes,
+      @NonNull OnCheckedStateChangedListener parentOnCheckedStateChangedListener) {
+    boolean allChecked = true;
+    boolean noneChecked = true;
+    for (CheckBox child : childrenCheckBoxes) {
+      if (!child.isChecked()) {
+        allChecked = false;
+      } else {
+        noneChecked = false;
+      }
+      if (!allChecked && !noneChecked) {
+        break;
+      }
     }
+    checkBoxParent.removeOnCheckedStateChangedListener(parentOnCheckedStateChangedListener);
+    if (allChecked) {
+      checkBoxParent.setChecked(true);
+    } else if (noneChecked) {
+      checkBoxParent.setChecked(false);
+    } else {
+      checkBoxParent.setCheckedState(MaterialCheckBox.STATE_INDETERMINATE);
+    }
+    checkBoxParent.addOnCheckedStateChangedListener(parentOnCheckedStateChangedListener);
+  }
 }
