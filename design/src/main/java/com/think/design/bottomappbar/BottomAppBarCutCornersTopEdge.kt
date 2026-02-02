3de8cfb3f44ed12f4a670bde7d89a24de53280e1
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-02-02 10:26:08 +0800

    [ThinkCode] #0 修改bottomAppbar示例

--- design/src/main/java/com/think/design/bottomappbar/BottomAppBarCutCornersTopEdge.kt
+++ design/src/main/java/com/think/design/bottomappbar/BottomAppBarCutCornersTopEdge.kt
@@ -1,65 +1,43 @@
-/*
- * Copyright 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.think.design.bottomappbar
 
-package com.think.design.bottomappbar;
+import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment
+import com.google.android.material.shape.ShapePath
 
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.shape.ShapePath;
+class BottomAppBarCutCornersTopEdge internal constructor(
+    private val fabMargin: Float,
+    roundedCornerRadius: Float,
+    private val cradleVerticalOffset: Float
+) : BottomAppBarTopEdgeTreatment(
+    fabMargin, roundedCornerRadius,
+    cradleVerticalOffset
+) {
+    override fun getEdgePath(
+        length: Float,
+        center: Float,
+        interpolation: Float,
+        shapePath: ShapePath
+    ) {
+        val fabDiameter = getFabDiameter()
+        if (fabDiameter == 0f) {
+            shapePath.lineTo(length, 0f)
+            return
+        }
 
-/**
- * A {@link BottomAppBar} top edge that works with a Diamond shaped {@link FloatingActionButton}
- */
-public class BottomAppBarCutCornersTopEdge extends BottomAppBarTopEdgeTreatment {
+        val diamondSize = fabDiameter / 2f
+        val middle = center + getHorizontalOffset()
 
-  private final float fabMargin;
-  private final float cradleVerticalOffset;
+        val verticalOffsetRatio = cradleVerticalOffset / diamondSize
+        if (verticalOffsetRatio >= 1.0f) {
+            shapePath.lineTo(length, 0f)
+            return
+        }
 
-  BottomAppBarCutCornersTopEdge(
-      float fabMargin, float roundedCornerRadius, float cradleVerticalOffset) {
-    super(fabMargin, roundedCornerRadius, cradleVerticalOffset);
-    this.fabMargin = fabMargin;
-    this.cradleVerticalOffset = cradleVerticalOffset;
-  }
+        shapePath.lineTo(middle - (fabMargin + diamondSize - cradleVerticalOffset), 0f)
 
-  @Override
-  @SuppressWarnings("RestrictTo")
-  public void getEdgePath(float length, float center, float interpolation, ShapePath shapePath) {
-    float fabDiameter = getFabDiameter();
-    if (fabDiameter == 0) {
-      shapePath.lineTo(length, 0);
-      return;
+        shapePath.lineTo(middle, (diamondSize - cradleVerticalOffset + fabMargin) * interpolation)
+
+        shapePath.lineTo(middle + (fabMargin + diamondSize - cradleVerticalOffset), 0f)
+
+        shapePath.lineTo(length, 0f)
     }
-
-    float diamondSize = fabDiameter / 2f;
-    float middle = center + getHorizontalOffset();
-
-    float verticalOffsetRatio = cradleVerticalOffset / diamondSize;
-    if (verticalOffsetRatio >= 1.0f) {
-      shapePath.lineTo(length, 0);
-      return;
-    }
-
-    shapePath.lineTo(middle - (fabMargin + diamondSize - cradleVerticalOffset), 0);
-
-    shapePath.lineTo(middle, (diamondSize - cradleVerticalOffset + fabMargin) * interpolation);
-
-    shapePath.lineTo(middle + (fabMargin + diamondSize - cradleVerticalOffset), 0);
-
-    shapePath.lineTo(length, 0);
-  }
 }
