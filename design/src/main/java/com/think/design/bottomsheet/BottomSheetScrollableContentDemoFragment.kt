f71a4f4c9d15c716b0c6debd45b9d94b8bcc5c7b
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-02-02 15:23:19 +0800

    Revert "[ThinkCode] #0 修改bottomSheet示例"
    This reverts commit 4c588abdf6dd19bd2edd4425d215806892a24d64.

--- design/src/main/java/com/think/design/bottomsheet/BottomSheetScrollableContentDemoFragment.kt
+++ design/src/main/java/com/think/design/bottomsheet/BottomSheetScrollableContentDemoFragment.kt
@@ -1,86 +1,100 @@
-package com.think.design.bottomsheet
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.annotation.SuppressLint
-import android.app.Dialog
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.annotation.LayoutRes
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.insets.Protection
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.internal.ViewUtils
-import com.google.android.material.internal.ViewUtils.RelativePadding
-import com.think.design.R
-import com.think.design.feature.DemoFragment
-import com.think.design.windowpreferences.WindowPreferencesManager
+package com.think.design.bottomsheet;
 
-class BottomSheetScrollableContentDemoFragment : DemoFragment() {
-    override fun onCreateDemoView(
-        layoutInflater: LayoutInflater, viewGroup: ViewGroup?, bundle: Bundle?
-    ): View {
-        val view = layoutInflater.inflate(this.demoContent, viewGroup, false /* attachToRoot */)
-        val button = view.findViewById<View>(R.id.bottomsheet_button)
-        button.setOnClickListener(View.OnClickListener { v: View? ->
-            BottomSheet().show(
-                getParentFragmentManager(),
-                ""
-            )
-        })
-        return view
+import com.think.design.R;
+
+import static com.google.android.material.bottomsheet.BottomSheetBehavior.getDefaultBottomGradientProtection;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.internal.ViewUtils;
+import com.think.design.feature.DemoFragment;
+import com.think.design.windowpreferences.WindowPreferencesManager;
+import java.util.Collections;
+
+/**
+ * A fragment that displays the a BottomSheet demo with vertical scrollable content for the Catalog
+ * app.
+ */
+public class BottomSheetScrollableContentDemoFragment extends DemoFragment {
+  @Override
+  public View onCreateDemoView(
+      LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
+    View view = layoutInflater.inflate(getDemoContent(), viewGroup, false /* attachToRoot */);
+    View button = view.findViewById(R.id.bottomsheet_button);
+    button.setOnClickListener(v -> new BottomSheet().show(getParentFragmentManager(), ""));
+    return view;
+  }
+
+  @LayoutRes
+  protected int getDemoContent() {
+    return R.layout.cat_bottomsheet_additional_demo_fragment;
+  }
+
+  /** A custom bottom sheet dialog fragment. */
+  @SuppressWarnings("RestrictTo")
+  public static class BottomSheet extends BottomSheetDialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+      // Set up BottomSheetDialog
+      BottomSheetDialog bottomSheetDialog =
+          new BottomSheetDialog(
+              getContext(), R.style.ThemeOverlay_Catalog_BottomSheetDialog_Scrollable);
+      new WindowPreferencesManager(requireContext())
+          .applyEdgeToEdgePreference(bottomSheetDialog.getWindow());
+      View content =
+          LayoutInflater.from(getContext())
+              .inflate(R.layout.cat_bottomsheet_scrollable_content, new FrameLayout(getContext()));
+      bottomSheetDialog.setContentView(content);
+      bottomSheetDialog.getBehavior().setPeekHeight(400);
+
+      View bottomSheetContent = content.findViewById(R.id.bottom_drawer_2);
+      ViewUtils.doOnApplyWindowInsets(
+          bottomSheetContent,
+          (v, insets, initialPadding) -> {
+            // Add the inset in the inner NestedScrollView instead to make the edge-to-edge behavior
+            // consistent - i.e., the extra padding will only show at the bottom of all content,
+            // i.e.,
+            // only when you can no longer scroll down to show more content.
+            bottomSheetContent.setPaddingRelative(
+                initialPadding.start,
+                initialPadding.top,
+                initialPadding.end,
+                initialPadding.bottom
+                    + insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
+            return insets;
+          });
+      bottomSheetDialog.setProtections(
+          Collections.singletonList(
+              getDefaultBottomGradientProtection(requireContext())));
+      return bottomSheetDialog;
     }
-
-    @get:LayoutRes
-    protected val demoContent: Int
-        get() = R.layout.cat_bottomsheet_additional_demo_fragment
-
-    /** A custom bottom sheet dialog fragment.  */
-    class BottomSheet : BottomSheetDialogFragment() {
-        @SuppressLint("RestrictedApi")
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            // Set up BottomSheetDialog
-            val bottomSheetDialog =
-                BottomSheetDialog(
-                    requireContext(), R.style.ThemeOverlay_Catalog_BottomSheetDialog_Scrollable
-                )
-            WindowPreferencesManager(requireContext())
-                .applyEdgeToEdgePreference(bottomSheetDialog.getWindow())
-            val content =
-                LayoutInflater.from(getContext())
-                    .inflate(
-                        R.layout.cat_bottomsheet_scrollable_content,
-                        FrameLayout(requireContext())
-                    )
-            bottomSheetDialog.setContentView(content)
-            bottomSheetDialog.getBehavior().setPeekHeight(400)
-
-            val bottomSheetContent = content.findViewById<View>(R.id.bottom_drawer_2)
-            ViewUtils.doOnApplyWindowInsets(
-                bottomSheetContent,
-                ViewUtils.OnApplyWindowInsetsListener { v: View?, insets: WindowInsetsCompat?, initialPadding: RelativePadding? ->
-                    // Add the inset in the inner NestedScrollView instead to make the edge-to-edge behavior
-                    // consistent - i.e., the extra padding will only show at the bottom of all content,
-                    // i.e.,
-                    // only when you can no longer scroll down to show more content.
-                    bottomSheetContent.setPaddingRelative(
-                        initialPadding!!.start,
-                        initialPadding.top,
-                        initialPadding.end,
-                        initialPadding.bottom
-                                + insets!!.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
-                    )
-                    insets
-                })
-            bottomSheetDialog.setProtections(
-                mutableListOf<Protection?>(
-                    BottomSheetBehavior.getDefaultBottomGradientProtection(requireContext())
-                )
-            )
-            return bottomSheetDialog
-        }
-    }
+  }
 }
--- design/src/main/java/com/think/design/bottomsheet/BottomSheetUnscrollableContentDemoFragment.kt
+++ design/src/main/java/com/think/design/bottomsheet/BottomSheetUnscrollableContentDemoFragment.kt
@@ -1,74 +1,90 @@
-package com.think.design.bottomsheet
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.annotation.SuppressLint
-import android.app.Dialog
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.annotation.LayoutRes
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.internal.ViewUtils
-import com.google.android.material.internal.ViewUtils.RelativePadding
-import com.think.design.R
-import com.think.design.feature.DemoFragment
-import com.think.design.windowpreferences.WindowPreferencesManager
+package com.think.design.bottomsheet;
 
-class BottomSheetUnscrollableContentDemoFragment : DemoFragment() {
-    override fun onCreateDemoView(
-        layoutInflater: LayoutInflater,
-        viewGroup: ViewGroup?,
-        bundle: Bundle?
-    ): View {
-        val view = layoutInflater.inflate(this.demoContent, viewGroup, false /* attachToRoot */)
-        val button = view.findViewById<View>(R.id.bottomsheet_button)
-        button.setOnClickListener(View.OnClickListener { v: View? ->
-            BottomSheet().show(
-                getParentFragmentManager(),
-                ""
-            )
-        })
-        return view
+import com.think.design.R;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.internal.ViewUtils;
+import com.think.design.feature.DemoFragment;
+import com.think.design.windowpreferences.WindowPreferencesManager;
+
+/**
+ * A fragment that displays the a BottomSheet demo with unscrollable content and text input for the
+ * Catalog app.
+ */
+public class BottomSheetUnscrollableContentDemoFragment extends DemoFragment {
+  @NonNull
+  @Override
+  public View onCreateDemoView(
+      @NonNull LayoutInflater layoutInflater,
+      @Nullable ViewGroup viewGroup,
+      @Nullable Bundle bundle) {
+    View view = layoutInflater.inflate(getDemoContent(), viewGroup, false /* attachToRoot */);
+    View button = view.findViewById(R.id.bottomsheet_button);
+    button.setOnClickListener(v -> new BottomSheet().show(getParentFragmentManager(), ""));
+    return view;
+  }
+
+  @LayoutRes
+  protected int getDemoContent() {
+    return R.layout.cat_bottomsheet_additional_demo_fragment;
+  }
+
+  /** A custom bottom sheet dialog fragment. */
+  @SuppressWarnings("RestrictTo")
+  public static class BottomSheet extends BottomSheetDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+      // Set up BottomSheetDialog
+      BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext());
+      new WindowPreferencesManager(requireContext()).applyEdgeToEdgePreference(bottomSheetDialog.getWindow());
+      bottomSheetDialog.setContentView(R.layout.cat_bottomsheet_unscrollable_content);
+      View bottomSheetInternal = bottomSheetDialog.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+      // BottomSheetBehavior.from(bottomSheetInternal).setPeekHeight(400);
+
+      Button closeButton = bottomSheetDialog.findViewById(R.id.close_icon);
+      closeButton.setOnClickListener(v -> bottomSheetDialog.dismiss());
+
+      View bottomSheetContent = bottomSheetInternal.findViewById(R.id.bottom_drawer_3);
+      ViewUtils.doOnApplyWindowInsets(bottomSheetContent, (v, insets, initialPadding) -> {
+        // Add the inset in the inner NestedScrollView instead to make the edge-to-edge behavior
+        // consistent - i.e., the extra padding will only show at the bottom of all content, i.e.,
+        // only when you can no longer scroll down to show more content.
+        bottomSheetContent.setPaddingRelative(
+            initialPadding.start,
+            initialPadding.top,
+            initialPadding.end,
+            initialPadding.bottom + insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
+        return insets;
+      });
+      return bottomSheetDialog;
     }
-
-    @get:LayoutRes
-    protected val demoContent: Int
-        get() = R.layout.cat_bottomsheet_additional_demo_fragment
-
-    /** A custom bottom sheet dialog fragment.  */
-    class BottomSheet : BottomSheetDialogFragment() {
-        @SuppressLint("RestrictedApi")
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            // Set up BottomSheetDialog
-            val bottomSheetDialog = BottomSheetDialog(getContext()!!)
-            WindowPreferencesManager(requireContext()).applyEdgeToEdgePreference(bottomSheetDialog.getWindow())
-            bottomSheetDialog.setContentView(R.layout.cat_bottomsheet_unscrollable_content)
-            val bottomSheetInternal =
-                bottomSheetDialog.findViewById<View?>(com.google.android.material.R.id.design_bottom_sheet)
-
-            // BottomSheetBehavior.from(bottomSheetInternal).setPeekHeight(400);
-            val closeButton = bottomSheetDialog.findViewById<Button?>(R.id.close_icon)
-            closeButton!!.setOnClickListener(View.OnClickListener { v: View? -> bottomSheetDialog.dismiss() })
-
-            val bottomSheetContent = bottomSheetInternal!!.findViewById<View>(R.id.bottom_drawer_3)
-            ViewUtils.doOnApplyWindowInsets(
-                bottomSheetContent,
-                ViewUtils.OnApplyWindowInsetsListener { v: View?, insets: WindowInsetsCompat?, initialPadding: RelativePadding? ->
-                    // Add the inset in the inner NestedScrollView instead to make the edge-to-edge behavior
-                    // consistent - i.e., the extra padding will only show at the bottom of all content, i.e.,
-                    // only when you can no longer scroll down to show more content.
-                    bottomSheetContent.setPaddingRelative(
-                        initialPadding!!.start,
-                        initialPadding.top,
-                        initialPadding.end,
-                        initialPadding.bottom + insets!!.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
-                    )
-                    insets
-                })
-            return bottomSheetDialog
-        }
-    }
+  }
 }
