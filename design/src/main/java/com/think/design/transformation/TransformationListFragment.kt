6db7c9b1f9af73fd83c745c127630704a02173ec
魏兴泰 <xingtai.wei@dreamsmart.com>
2026-02-06 15:31:40 +0800

    [ThinkCode] #0 增加控件列表目录

---
+++ design/src/main/java/com/think/design/transformation/TransformationListFragment.kt
@@ -0,0 +1,41 @@
+package com.think.design.transformation
+
+import androidx.fragment.app.Fragment
+import com.think.design.R
+import com.think.design.base.BaseListingFragment
+import com.think.design.base.Example
+import com.think.design.checkbox.CheckBoxMainFragment
+
+/**
+ * author : Samuel
+ * e-mail : xingtai.wei@dreamsmart.com
+ * time   : 2026/2/3 10:23
+ * desc   :
+ */
+class TransformationListFragment : BaseListingFragment() {
+
+    override fun getTitleResId(): Int {
+        return R.string.checkbox_title
+    }
+
+    override fun getDescResId(): Int {
+        return R.string.checkbox_description
+    }
+
+    override fun onCreateMainExample(): Example {
+        return object : Example() {
+            override fun buildDestinationId(): Int {
+                return R.id.navigation_checkbox_main
+            }
+
+            override fun buildFragment(): Fragment {
+                return CheckBoxMainFragment()
+            }
+        }
+    }
+
+    override fun onCreateExtraList(): List<Example> {
+        val extrasList = mutableListOf<Example>()
+        return extrasList
+    }
+}
